e17426fccc580d5df28c213d6a90be344867e3d7
VincentNguyen39 <91209096+VincentNguyen23@users.noreply.github.com>
2022-01-13 17:31:23 +0000

    Initial commit

---
+++ src/test/java/edu/bothell/handlingformsubmission/HandlingFormSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bothell.handlingformsubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlingFormSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
